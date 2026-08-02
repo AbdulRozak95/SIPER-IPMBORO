a813fdbe687c4ed4932f3e0d68c950b052e438d9
AbdulRozak95 <abdurrozaqkhoirudin95@gmail.com>
2026-08-03 06:56:38 +0700

    SIPER IPM BORO

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
    hasil Deploy dari Google Apps Script (lihat Code.gs).
 ======================================================= */
 
-const API_URL = "https://script.google.com/macros/s/AKfycbxn0ndYKRGvjrCz6Dj_7MA3JWut5BU5VeaePKrqBkKJeBixtx2wzqa8uucBrOnQsXbVow/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzrmK_xd6YkL68CZdRAHSupjeuLhWYUDT9237B_rSc9gkd_7Z9nPBKc4XvMTRE3Cw39AQ/exec";
 
 let suratData = [];
 let editId = null;
