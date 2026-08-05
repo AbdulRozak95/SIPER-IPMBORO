ae23a399d085b8279b23a4a0c92bc17519208f0e
AbdulRozak95 <abdurrozaqkhoirudin95@gmail.com>
2026-08-05 15:32:32 +0700

    SIPER IPM BORO

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
    dari cache).
 ======================================================= */
 
-const CACHE_NAME = "siper-ipm-cache-v8";
+const CACHE_NAME = "siper-ipm-cache-v9";
 const FILE_TERCACHE = [
   "./index.html",
   "./style.css",
